7b9c9b920634f127b7c8c45a13c3bc0d7f7efbe6
Yasuharu Nakano <ynak@jggug.org>
2018-05-01 16:13:12 +0900

    Add perms into lib_misc

--- code/lib_misc.erl
+++ code/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([for/3, qsort/1, pythag/1]).
+-export([for/3, qsort/1, pythag/1, perms/1]).
 
 for(Max, Max, F) -> [F(Max)];
 for(I, Max, F)   -> [F(I)|for(I+1, Max, F)].
@@ -18,3 +18,6 @@ pythag(N) ->
         A + B + C =< N,
         A*A + B*B =:= C*C
     ].
+
+perms([]) -> [[]];
+perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
