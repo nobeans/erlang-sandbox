97bb30d13fe708bd0e22c9fc91d74fea5c57d47f
Yasuharu Nakano <ynak@jggug.org>
2018-05-02 19:26:40 +0900

    Add demo3 into try_test

--- code/try_test.erl
+++ code/try_test.erl
@@ -1,5 +1,5 @@
 -module(try_test).
--export([generate_exception/1, demo1/0, demo2/0]).
+-export([generate_exception/1, demo1/0, demo2/0, demo3/0]).
 
 generate_exception(1) -> a;
 generate_exception(2) -> throw(a);
@@ -22,3 +22,9 @@ catcher(N) ->
 demo2() ->
     [{I, {catch generate_exception(I)}} || I <- [1,2,3,4,5]].
 
+demo3() ->
+    try generate_exception(5)
+    catch
+        error:X ->
+            {X, erlang:get_stacktrace()}
+    end.
